d91d745eea98aa7c96e341b0cab93294142b2339
Jason Thorne <jthorne@magiclamp.com.au>
2018-01-10 12:34:26 +1100

    Need to take into account if the queued task was added in the last 5 minutes and completed or is running

--- async_que/management/commands/queue_cron_tasks.py
+++ async_que/management/commands/queue_cron_tasks.py
@@ -1,12 +1,16 @@
-import datetime
+import datetime as dt
 import pytz
 
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.db.models import Q
 from django.utils.timezone import localtime, now, activate
 
-from async_que.helpers import TaskStatusEnum
-from async_que.models import QueuedTask, CronTask
+from common.helpers import TaskStatusEnum
+from common.models import QueuedTask, CronTask
+from common.utils import get_last_day
+
+QUE_LOOP_MINS = 5 # You should call this job every QUE_LOOP_MINS in crontab
 
 class Command(BaseCommand):
 
@@ -17,13 +21,20 @@ class Command(BaseCommand):
         cron_jobs = CronTask.objects.filter(active=True)
         for cj in cron_jobs.all():
             cjminute = cj.minute or 0
-            if cj.hour != None and (hrn.hour > cj.hour or cj.hour == hrn.hour and hrn.minute >= cjminute):
-                # 23 hours before assuming they only run daily
-                last_run = hrn - datetime.timedelta(hours=23, minutes=59)
-                cron_task_exists = QueuedTask.objects.filter(
-                    command=cj.command, 
-                    created_on__gt=last_run,
-                    created_on__lte=hrn
-                    ).exclude(crontask=None).exists()
-                if not cron_task_exists:
-                    new_task = cj.queued_tasks.create(command=cj.command)
+            chron_run_after = dt.time(hour=cj.hour, minute=cjminute)
+            chron_run_before = (dt.datetime.combine(dt.date(1,1,1),chron_run_after) + dt.timedelta(minutes=QUE_LOOP_MINS+1)).time()
+            has_already_run = dt.datetime.now() - dt.timedelta(minutes=QUE_LOOP_MINS+1) 
+            
+            # Don't duplicate a chron added pending task on the queue
+            cron_task_exists = QueuedTask.objects \
+               .filter(crontask=cj) \
+               .filter(Q(status=TaskStatusEnum.PENDING) | Q(status=TaskStatusEnum.RUNNING) | Q(last_modified__gte=has_already_run)) \
+               .exists()
+            if (cj.day_of_month != None and hrn.day != cj.day_of_month) or \
+               (cj.day_of_week != None and hrn.weekday() != cj.day_of_week) or \
+               cron_task_exists:
+                continue
+
+            if  hrn.time() >= chron_run_after and hrn.time() <= chron_run_before:
+                new_task = cj.queued_tasks.create(command=cj.command)
+                print('Added %s to queued tasks' % cj.command)
